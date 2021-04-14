8c2c5c60714743162cfddb1813cfb88fb24cad3e
Samuel Holloway <samuel.holloway@salesforce.com>
2021-04-14 11:34:10 -0500

    Added a basic UI.

--- queueing_sims/fixed-window-ui.py
+++ queueing_sims/fixed-window-ui.py
@@ -9,10 +9,6 @@
 # - The ability to track the queue depth.
 # - A simple CLI UI for observing the simulation behavior.
 #
-# Considerations
-# - A warm up period?..
-# - Migrate to OO implementation.
-#
 # UI
 # - Simulation Progress Bar based on Sim Duration
 # - Inbound Request Rate
@@ -22,30 +18,39 @@
 # - Total Requests Processed
 ###############################################################################
 
-import simpy
+from rich import print
+from rich import box
 from rich.console import Console
 from rich.layout import Layout
 from rich.live import Live
 from rich.panel import Panel
+from rich.console import RenderGroup
+from rich.progress import Progress
+from rich.table import Table
+import simpy
 
 import math
 from statistics import mean
 import random
 
+console = Console()
+
 # The UOM for time in the simulation is 1 tick = 1 second.
 MINUTE = 60 # 1 minute is 60 seconds
-DURATION = 60 * 60 * 1 # Seconds * Minutes * Hours
+DURATION = 60 * 60 * 10 # Seconds * Minutes * Hours
 WINDOW_SIZE = MINUTE * 1
 
 # 500 requests/min => 8.333333333333334 Requests/sec
 # 1 request = 0.12 Seconds
 AVG_REQUEST_ARRIVAL_SPEED = 0.12
-MAX_THRESHOLD = 500 # The maximum number of requests that can be processed in the window.
+MAX_THRESHOLD = 100 # The maximum number of requests that can be processed in the window.
 
+UI_REFRESH_RATE = 10 # The number of simulation ticks to update the UI.
 
 metrics = {
+  "current_sim_tick": 0,
   "requests_submitted": 0,
-  "requests_processed":0,
+  "requests_processed": 0,
   "threshold_exceeded_wait_times": []
 }
 
@@ -92,24 +97,64 @@ def find_window(now: int, window_size: int ) -> tuple[int, int]:
   window_end = window_start + window_size 
   return window_start, window_end
 
+def update_ui(env, store, ui_layout, sim_progress, sim_task):
+  while True:
+    # 1. Update the simulation progress bar.
+    previous_tick = metrics["current_sim_tick"]
+    metrics["current_sim_tick"] = math.floor(env.now)
+    sim_progress.update(sim_task, advance = metrics["current_sim_tick"] - previous_tick)
+    # ui_layout["upper"].update(Panel.fit(sim_progress, title="Simulation Progress"))
+    ui_layout["upper"].update(sim_progress)
+    
+    # 2. Determine the number of requests waiting to be processed.
+    pending_requests_count = len(store.items)
+    pending_requests_msg = f"Pending Requests Count: {pending_requests_count}"
+
+    # 3. Collect the simulation's metrics.
+    rate_exceeded_count = len(metrics["threshold_exceeded_wait_times"])
+    avg_wait_time = 0
+    if rate_exceeded_count > 0:
+      avg_wait_time = mean(metrics["threshold_exceeded_wait_times"])
+
+
+    # 5. Build a table to represent the metrics.
+    sim_table = Table("Requests Submitted", "Requests Processed", "Rate Exceeded Count", "Avg Wait Time (s)", 
+                      title="Simulation Metrics", box=box.SIMPLE_HEAVY)
+    sim_table.add_row(str(metrics['requests_submitted']), 
+      str(metrics['requests_processed']), 
+      str(rate_exceeded_count), 
+      str(avg_wait_time))
+
+    # 6. Update the lower panel.
+    ui_layout["lower"].update(RenderGroup(pending_requests_msg,sim_table))
+
+    yield env.timeout(UI_REFRESH_RATE)
+
+def create_ui_layout(sim_progress):
+  layout = Layout()
+  # Divide the "screen" in to two rows
+  layout.split_column(
+    Layout(name="upper"),
+    Layout(name="lower")
+  )
+  layout["upper"].size = 1
+  # layout["lower"].size = 6
+  return layout
+
 def main():
-  env = simpy.Environment()
-  store = simpy.Store(env)
-  env.process(generate_requests(env, AVG_REQUEST_ARRIVAL_SPEED, store))
-  env.process(fixed_widow_processor(env, WINDOW_SIZE, MAX_THRESHOLD, store))
-  env.run(until = DURATION)
+  sim_progress = Progress(expand=False)
+  ui_layout = create_ui_layout(sim_progress)
+  with Live(ui_layout, refresh_per_second=10, screen=True):
+    sim_task = sim_progress.add_task("[red]Running Simulation...", total = DURATION)
+    env = simpy.Environment()
+    store = simpy.Store(env)
+    env.process(generate_requests(env, AVG_REQUEST_ARRIVAL_SPEED, store))
+    env.process(fixed_widow_processor(env, WINDOW_SIZE, MAX_THRESHOLD, store))
+    env.process(update_ui(env, store, ui_layout, sim_progress, sim_task))
+    env.run(until = DURATION)
 
-  print(f"Requests Submitted: {metrics['requests_submitted']}")
-  print(f"Requests Processed: {metrics['requests_processed']}")
-
-  rate_exceeded_count = len(metrics["threshold_exceeded_wait_times"])
-  print(f"Rate Exceeded Count: {rate_exceeded_count}")
-  if rate_exceeded_count > 0:
-    avg_wait_time = mean(metrics["threshold_exceeded_wait_times"])
-    print(f"Avg Wait Time: {avg_wait_time}")
-
-  for i in metrics["threshold_exceeded_wait_times"]:
-    print(i)
+  # After the simulation is done: Display the final UI
+  print(ui_layout)
 
 if __name__ == "__main__":
   main()
